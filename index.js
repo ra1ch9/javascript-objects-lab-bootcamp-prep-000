4bee31b34f2582fd16b816214e02a1649f197925
rachel schest <rschest@gmail.com>
2017-11-01 19:16:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var recipes = new Object();
 function updateObjectWithKeyAndValue(object, [key], value){
-
   return Object.assign({}, object, {[key]: value})
 
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
+}
+function deleteFromObjectByKey(object, key){
+  var newRecipe = Object.assign({}, object)
+  delete newRecipe
 }
